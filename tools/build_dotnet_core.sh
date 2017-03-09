bbba565b3bb043188753c6613aaf9e84f7eb7421
Damon Barry <damonb@microsoft.com>
2017-03-08 16:59:56 -0800

    Fix config argument to 'dotnet build'
    MSBUILD : error MSB1003: Specify a project or solution file. The current working directory does not contain a project or solution file.

--- tools/build_dotnet_core.sh
+++ tools/build_dotnet_core.sh
@@ -31,10 +31,10 @@ do
     fi
 done
 
-if [ $build_config == 0 ]
+if [ "$build_config" = "0" ]
 then
     build_config=
-elif [ $build_config == 1 ]
+elif [ "$build_config" = "1" ]
 then
     exit 1
 fi
@@ -66,7 +66,7 @@ done
 
 for project in $projects_to_build
 do
-    dotnet build $project
+    dotnet build $build_config $project
     [ $? -eq 0 ] || exit $?
 done
 
